28cfc0bf7f11358241133a71a0ba0ec044b12c64
Carson Zhang <carsonzhang4@gmail.com>
2024-08-31 16:16:56 +0200

    pretty plots

--- ClusterDE/seminar_paper-custom/toy_double_dipping_plots.R
+++ ClusterDE/seminar_paper-custom/toy_double_dipping_plots.R
@@ -3,6 +3,7 @@ library(mvtnorm)
 library(ggplot2)
 library(tidyverse)
 library(ggpubr)
+library(RColorBrewer)
 
 # Set random seed for reproducibility
 set.seed(123)
@@ -24,7 +25,7 @@ df$cluster <- as.factor(kmeans_result$cluster)
 
 p_mvn_clustered <- ggplot(df, aes(x = X1, y = X2, color = cluster)) +
   geom_point(size = 1, shape = 1) +
-  scale_color_manual(values = c("red", "blue")) +
+  scale_color_brewer(palette = "Accent") +
   labs(title = "After clustering")
 
 ggarrange(p_mvn_gray, p_mvn_clustered) |>
@@ -44,13 +45,13 @@ stat.test <- stat.test |>
 
 # Create box plot using ggplot2
 p1 <- ggboxplot(df, x = "cluster", y = "X1",
-                color = "cluster") +
+                color = "cluster", palette = get_palette("Accent", 3)) +
   stat_pvalue_manual(stat.test, label = "p.format")
 p2 <- ggboxplot(df, x = "cluster", y = "X2",
-                color = "cluster") +
+                color = "cluster", palette = get_palette("Accent", 3)) +
   stat_pvalue_manual(stat.test, label = "p.format")
-ggarrange(p1, p2, 
-          common.legend = TRUE) |>
+
+ggarrange(p1, p2, common.legend = TRUE) |>
   ggexport(filename = "../seminar_paper-custom/figures/fig-boxplot-S15.png",
            width = 800, height = 400)
 
